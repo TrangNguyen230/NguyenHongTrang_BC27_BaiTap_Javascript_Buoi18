9932e52d418f675717efc86d18cdcc373a62c9e5
TrangNguyen230 <htrang28396@gmail.com>
2022-05-24 16:10:55 +0700

    complete

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var numbs = [];
 
 // Hàm DOM giá trị nhập
-function getValue(inputNumb) {
+function getValue() {
   var inputNumb = +document.getElementById("inputNumb").value;
   return inputNumb;
 }
+
+//==========================================================================
 
 // Hàm thêm số
 function addNumb() {
@@ -14,9 +16,11 @@ function addNumb() {
   numbs.push(n);
 
   // Hiển thị kết quả
-  document.querySelector(".alert-secondary").style.display = "block";
-  document.querySelector(".alert-secondary").innerHTML = numbs;
+  document.querySelector("#arr1").style.display = "block";
+  document.querySelector("#arr1").innerHTML = numbs;
 }
+
+//==========================================================================
 
 // Hàm tính tổng các số dương
 function sumCal() {
@@ -36,6 +40,8 @@ function sumCal() {
   ).innerHTML = `Tổng các số dương là: ${sum}`;
 }
 
+//=========================================================================
+
 // Hàm đếm số dương
 function countPositiveNumb() {
   var count = 0;
@@ -52,6 +58,8 @@ function countPositiveNumb() {
   ).innerHTML = `Số lượng các số dương là: ${count}`;
 }
 
+//=========================================================================
+
 // Hàm tìm số nhỏ nhất
 function getSmallestNumb() {
   numbs.sort(sortNumb);
@@ -63,10 +71,14 @@ function getSmallestNumb() {
   ).innerHTML = `Số nhỏ nhất là: ${numbs[0]}`;
 }
 
+// =======================================================================
+
 // Hàm sắp xếp từ nhỏ tới lớn
 function sortNumb(a, b) {
   return a - b;
 }
+
+// ========================================================================
 
 // Hàm tìm số dương nhỏ nhất
 function getSmallestPositiveNumb() {
@@ -91,6 +103,8 @@ function getSmallestPositiveNumb() {
   }
 }
 
+// ========================================================================
+
 // Hàm tìm số chẵn cuối cùng
 function getLastEven() {
   var evens = [];
@@ -112,6 +126,8 @@ function getLastEven() {
   }
 }
 
+// =======================================================================
+
 // Hàm sắp xếp theo thứ tự tăng dần
 function ascendingSort() {
   numbs.sort(sortNumb);
@@ -123,7 +139,136 @@ function ascendingSort() {
   ).innerHTML = `Sắp xếp theo thứ tự tăng dần: ${numbs} `;
 }
 
+// ========================================================================
+
 // Hàm tìm số nguyên tố đầu tiên
 function getFirstPrimeNumb() {
-    
+  var primeNumbs = [];
+  for (var i = 0; i < numbs.length; i++) {
+    if (isPrime(numbs[i])) {
+      primeNumbs.push(numbs[i]);
+    }
+  }
+
+  if (primeNumbs.length === 0) {
+    document.querySelector(".alert-success").style.display = "block";
+    document.querySelector(".alert-success").innerHTML = primeNumbs.indexOf();
+  } else {
+    document.querySelector(".alert-success").style.display = "block";
+    document.querySelector(
+      ".alert-success"
+    ).innerHTML = `Số nguyên tố đầu tiên là: ${primeNumbs[0]}`;
+  }
 }
+
+// Hàm xác định có là số nguyên tố hay không
+function isPrime(x) {
+  if (x < 2) {
+    return false;
+  }
+
+  for (var j = 2; j <= Math.sqrt(x); j++) {
+    if (x % j === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//==========================================================================
+
+// Hàm so sánh số lượng số âm và số dương
+function compare() {
+  var p = isPositiveNumb();
+  var n = isNegativeNumb();
+
+  if (p.length > n.length) {
+    document.querySelector(".alert-success").style.display = "block";
+    document.querySelector(".alert-success").innerHTML = `Số dương > Số âm`;
+  } else if(p.length < n.length) {
+    document.querySelector(".alert-success").style.display = "block";
+    document.querySelector(".alert-success").innerHTML = `Số dương < Số âm`;
+  } else{
+    document.querySelector(".alert-success").style.display = "block";
+    document.querySelector(".alert-success").innerHTML = `Số dương = Số âm`;
+  }
+}
+
+// Hàm xác định số dương
+function isPositiveNumb() {
+  var positiveNumbs = [];
+  for (var i = 0; i < numbs.length; i++) {
+    if (numbs[i] >= 0) {
+      positiveNumbs.push(numbs[i]);
+    }
+  }
+  return positiveNumbs;
+}
+
+// Hàm xác định số âm
+function isNegativeNumb() {
+  var negativeNumbs = [];
+  for (var i = 0; i < numbs.length; i++) {
+    if (numbs[i] < 0) {
+      negativeNumbs.push(numbs[i]);
+    }
+  }
+  return negativeNumbs;
+}
+
+//==========================================================================
+
+// Hàm đổi vị trí số trong mảng
+function changePosition() {
+  var e1 = document.getElementById("inputPosition1").value
+  var e2 = document.getElementById("inputPosition2").value
+
+  var temp = numbs[e1]
+  numbs[e1] = numbs[e2]
+  numbs[e2] = temp
+
+  document.querySelector(".alert-danger").style.display = "block";
+  document.querySelector(".alert-danger").innerHTML = numbs;
+}
+
+//=====================================================================
+
+// Tạo biến mảng rỗng
+var realNumbs = [];
+
+// Hàm DOM giá trị nhập
+function getNumbValue() {
+  var inputRealNumb = +document.getElementById("inputRealNumb").value;
+  return inputRealNumb;
+}
+
+//==========================================================================
+
+// Hàm thêm số
+function addRealNumb() {
+  var n = getNumbValue();
+  // Dùng thuộc tính push để thêm giá trị nhập vào mảng numbs
+  realNumbs.push(n);
+
+  // Hiển thị kết quả
+  document.querySelector("#arr2").style.display = "block";
+  document.querySelector("#arr2").innerHTML = realNumbs;
+}
+
+// Hàm đếm số nguyên trong mảng
+function countInteger() {
+  var count = 0
+  for (var i = 0; i < realNumbs.length; i++) {
+    if(Number.isInteger(realNumbs[i])) {
+      count++
+    }
+  }
+
+  // Hiển thị kết quả
+  document.querySelector(".alert-warning").style.display = "block";
+  document.querySelector(".alert-warning").innerHTML = `Số lượng số nguyên là: ${count}`;
+}
+
+
+
+
